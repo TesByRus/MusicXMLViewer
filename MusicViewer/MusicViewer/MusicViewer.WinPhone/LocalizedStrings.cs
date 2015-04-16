cd91fdfc4bb159e9259c63cbdcf0ec2215314a14
Yusupov Danil <yusupdan@gmail.com>
2015-04-16 09:43:00 +0500

    Create project agane

---
+++ MusicViewer/MusicViewer/MusicViewer.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using MusicViewer.WinPhone.Resources;
+
+namespace MusicViewer.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
